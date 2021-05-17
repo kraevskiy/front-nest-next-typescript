108df6d625f3ed08a58f9e476bd8100c208e7bd3
kraevskiy <rambutan@educonlines.com>
2021-05-17 09:50:22 +0300

    add Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import {Htag} from "../components";
+import {Htag, Button} from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,8 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">Text h1</Htag>
       <Htag tag="h2">Text h1</Htag>
       <Htag tag="h3">Text h1</Htag>
+      <Button appearance="primary">sdfsdfsdf</Button>
+      <Button appearance="ghost">sdfsdfsdf</Button>
     </div>
   );
 }
