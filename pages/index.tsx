54661b0cfafd5c92954ce06e89a936cf4b949abb
kraevskiy <rambutan@educonlines.com>
2021-05-17 09:28:47 +0300

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import {Htag} from "../components";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Htag tag="h1">Text h1</Htag>
+      <Htag tag="h2">Text h1</Htag>
+      <Htag tag="h3">Text h1</Htag>
+    </div>
+  );
+}
